156ee6ab1f0679288dc11c06cea0fc253b89291e
ghufyoo <ghuf.yoo@gmail.com>
2023-06-19 15:54:33 +0800

    push to main

---
+++ build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_foundation.build/Objects-normal/arm64/path_provider_foundation-dummy.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/ghufranismail/Desktop/flutter_starter_template/build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_foundation.build/module.modulemap \
+  /Users/ghufranismail/Desktop/flutter_starter_template/macos/Pods/Target\ Support\ Files/path_provider_foundation/path_provider_foundation-dummy.m \
+  /Users/ghufranismail/Desktop/flutter_starter_template/macos/Pods/Target\ Support\ Files/path_provider_foundation/path_provider_foundation-prefix.pch
---
+++ build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_foundation.build/Objects-normal/arm64/path_provider_foundation_vers.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/ghufranismail/Desktop/flutter_starter_template/build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_foundation.build/module.modulemap \
+  /Users/ghufranismail/Desktop/flutter_starter_template/build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_foundation.build/DerivedSources/path_provider_foundation_vers.c
